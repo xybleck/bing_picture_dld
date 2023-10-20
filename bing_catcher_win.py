18792cfdfaf63ea466c298cedb8a007b6f930a87
XYbleck <43821011+xybleck@users.noreply.github.com>
2023-10-20 23:34:24 +0200

    Update bing_catcher_win.py

--- bing_catcher_win.py
+++ bing_catcher_win.py
@@ -3,7 +3,14 @@
 import requests
 import re
 import os
+import sys
 import datetime
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--directory', type=str, help="Folder path to save bing wallpaper")
+args = parser.parse_args()
 
 TYEAR = datetime.datetime.now().year
 TYEAR = str(TYEAR) if TYEAR > 9 else "0"+str(TYEAR)
@@ -13,20 +20,56 @@ TDAY = datetime.datetime.now().day
 TDAY = str(TDAY) if TDAY > 9 else "0"+str(TDAY)
 BING_URL = "https://bing.com"
 BING_WALLPAPER_FILENAME = TYEAR + TMONTH + TDAY + "_bing_wallpaper.jpeg"
-BING_WALLPAPER_LOCAL_PATH = os.path.join('C:\\Users\\j_del\\Pictures\\BingWallpaper', BING_WALLPAPER_FILENAME)
 
+if args.directory:
+    BING_WALLPAPER_LOCAL_PATH = os.path.join(args.directory, BING_WALLPAPER_FILENAME)
+else:
+   parser.print_help()
+   sys.exit(1)
+
+SUCCESS = "Wallpaper download success: " + BING_WALLPAPER_LOCAL_PATH
+
+
+def bing_wallpaper_url() -> str:
+    try:
+        req = requests.get(BING_URL)
+        if req.status_code == 200:
+            for _ in req.text.split(" "):
+                if re.match(r'^url\(', _):
+                    bing_wallpaper_url = "https://bing.com" + str(_.split("(")[1].split(")")[0])
+                    return bing_wallpaper_url
+        else:
+            raise SystemError("HTTPS request failed resolving wallpaper url")
+        
+    except Exception as e:
+        print("Error: " + e)
+        sys.exit(1)
+
+def bing_wallpaper_img(url: str) -> bool:
+    try:
+        req = requests.get(url)
+        if req.status_code == 200:
+            f = open(BING_WALLPAPER_LOCAL_PATH, 'wb')
+            f.write(req.content)
+            f.close()
+            return True
+        else:
+            raise SystemError("HTTPS request failed downloading wallpaper img")
+            
+    except Exception as e:
+        print("Error: " + e)
+        sys.exit(1)
+        
 
 def main():
-  r = requests.get(BING_URL)
-  if r.status_code == 200:
-    for _ in r.text.split(" "):
-      if re.match(r'^url\(', _):
-        bing_wallpaper_url = "https://bing.com" + str(_.split("(")[1].split(")")[0])
-        r = requests.get(bing_wallpaper_url)
-        if r.status_code == 200:
-          f = open(BING_WALLPAPER_LOCAL_PATH, 'wb')
-          f.write(r.content)
-          f.close()
+    try:
+        wu = bing_wallpaper_url()
+        bing_wallpaper_img(wu)
+        print(SUCCESS)
+    
+    except Exception as e:
+        print("Error: " + e)
+        sys.exit(0)
 
 
 if __name__ == "__main__":
